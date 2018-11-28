8f76d3e0a7bc3b89ab1b6cd722d54191d3428f08
Matthew Kelly <badgerati42@gmail.com>
2018-11-28 21:57:45 +0000

    fixes for chocolatey

--- packers/choco/tools/ChocolateyUninstall.ps1
+++ packers/choco/tools/ChocolateyUninstall.ps1
@@ -1,18 +1,33 @@
-# Determine which Program Files path to use
-if (![string]::IsNullOrWhiteSpace($env:ProgramFiles)) {
-    $modulePath = Join-Path $env:ProgramFiles (Join-Path 'WindowsPowerShell' 'Modules')
-}
-else {
-    $modulePath = Join-Path ${env:ProgramFiles(x86)} (Join-Path 'WindowsPowerShell' 'Modules')
-}
-
-# Delete PoshHosts module
-$hostsModulePath = Join-Path $modulePath 'PoshHosts'
-if (Test-Path $hostsModulePath)
+function Remove-PoshHostsModule($path)
 {
-    Write-Host 'Deleting PoshHosts module directory'
-    Remove-Item -Path $hostsModulePath -Recurse -Force | Out-Null
-    if (!$?) {
-        throw "Failed to delete: $hostsModulePath"
+    $path = Join-Path $path 'PoshHosts'
+    if (Test-Path $path)
+    {
+        Write-Host "Deleting PoshHosts module directory: $($path)"
+        Remove-Item -Path $path -Recurse -Force | Out-Null
+        if (!$?) {
+            throw "Failed to delete: $path"
+        }
     }
 }
+
+
+# Determine which Program Files path to use
+$progFiles = [string]$env:ProgramFiles
+if (!(Test-Path $progFiles)) {
+    $progFiles = [string]${env:ProgramFiles(x86)}
+}
+
+# Remove PS Module
+# Set the module path
+$modulePath = Join-Path $progFiles (Join-Path 'WindowsPowerShell' 'Modules')
+
+# Delete PoshHosts module
+Remove-PoshHostsModule $modulePath
+
+# Remove PS-Core Module
+# Set the module path
+$modulePath = Join-Path $progFiles (Join-Path 'PowerShell' 'Modules')
+
+# Delete PoshHosts module
+Remove-PoshHostsModule $modulePath
